20e5d0c87d6566c55e5c865fd0b17a7354267043
jasonwynn10 <jasonwynn10@gmail.com>
2016-05-09 10:00:19 -0400

    Update Main.php

--- src/LostTeam/Main.php
+++ src/LostTeam/Main.php
@@ -1,7 +1,7 @@
 <?php
 namespace LostTeam;
 
-//Pocketmine
+use LostTeam\events\JoinEvent;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;	
@@ -9,37 +9,28 @@ use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat as TF;
 
-// LostTeam
-
-use LostTeam\events\JoinEvent;
-
 class Main extends PluginBase implements Listener {
-  
   public function onEnable() {
     $this->getServer()->getLogger()->notice(TF::GREEN."Enabled!");
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getServer()->getPluginManager()->registerEvents(new JoinEvent($this), $this);
   }
-   public function onCommand(CommandSender $sender, Command $command, $label, array $args)
-  {
-
-          if(strtolower($command) === "bot"){
-            if($args[0] === "version") {
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+    if(strtolower($command) === "bot") {
+      if($args[0] === "version") {
         $sender->sendMessage(TF::AQUA."LostBot:".TF::GREEN." Im running on 1.0.0 LostTeams chip.");
-       }
-            if($args[0] === "disable"){
+      }
+      if($args[0] === "disable") {
         $sender->sendMessage(TF::AQUA."LostBot:".TF::RED." Disabled.");
         $this->getPluginManager()->disablePlugin($this)
-       }
-            if($args[0] === "help"){
+      }
+      if($args[0] === "help") {
         $sender->sendMessage(TF::YELLOW."LostBot: /bot help");
         $sender->sendMessage(TF::YELLOW."LostBot: /bot version");
         $sender->sendMessage(TF::YELLOW."LostBot: /bot disable");
-
-       }
-
-
-     }
-
-   }
-
+      }
+    }
+    public function onDisable() {
+      $this->getServer()->getLogger()->notice(TF::GREEN."Disabled!");
+    }
+  }
 }
