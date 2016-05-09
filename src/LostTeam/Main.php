1ccbd65a15345ea54c56e11075d5eb2610a7ce0f
Daniel <daniel.barinak@gmail.com>
2016-05-09 04:35:43 +0200

    Update Main.php

--- src/LostTeam/Main.php
+++ src/LostTeam/Main.php
@@ -5,6 +5,7 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat as TF;
+use LostTeam\events\JoinEvent;
 
 class Main extends PluginBase implements Listener {
   
@@ -13,4 +14,30 @@ class Main extends PluginBase implements Listener {
     $this->getServer()->getLogger()->notice(TF::GREEN."Enabled!");
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
+   public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+  {
+
+          if(strtolower($command) === "bot"){
+            if($args[0] === "version") {
+
+      
+
+       }
+            if($args[0] === disable){
+
+
+
+       }
+            if($args[0] === help){
+        $sender->sendMessage(TextFormat::YELLOW."Help: /bot help");
+        $sender->sendMessage(TextFormat::YELLOW."Version: /bot version");
+        $sender->sendMessage(TextFormat::YELLOW."Exit: /bot disable");
+
+       }
+
+
+     }
+
+   }
+
 }
