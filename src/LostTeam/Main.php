eeaeef4e04678b8cd85b5e63b1c08e56c8a0d6a2
Daniel <daniel.barinak@gmail.com>
2016-05-09 04:55:24 +0200

    Update Main.php

--- src/LostTeam/Main.php
+++ src/LostTeam/Main.php
@@ -1,16 +1,21 @@
 <?php
 namespace LostTeam;
+
+//Pocketmine
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;	
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat as TF;
+
+// LostTeam
+
 use LostTeam\events\JoinEvent;
 
 class Main extends PluginBase implements Listener {
   
   public function onEnable() {
-    $this->getCfg();
     $this->getServer()->getLogger()->notice(TF::GREEN."Enabled!");
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
