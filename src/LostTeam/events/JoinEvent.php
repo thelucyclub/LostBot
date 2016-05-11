6c264d55651d3821bd9f6074430358bcdd63af81
Daniel <daniel.barinak@gmail.com>
2016-05-11 10:45:30 +0200

    Update JoinEvent.php

--- src/LostTeam/events/JoinEvent.php
+++ src/LostTeam/events/JoinEvent.php
@@ -5,14 +5,14 @@ use LostTeam\Main;
 
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as TF;
-use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerJoimEvent;
 
 class JoinEvent implements Listener {
     public $plugin;
     public function __construct(Main $plugin) {
         $this->plugin = $plugin
     }
-    public function onChat(PlayerChatEvent $event){
+    public function onJoin(PlayerJoinEvent $event){
         $this->getServer()->broadcastMessage("Welcome ".$event->getPlayer()->getName()."!");
     }
 }
